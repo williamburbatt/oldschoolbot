a1a8a1f7043855a52d3af0ee0974e730f6aa59d8
gc <gc@users.noreply.github.com>
2020-04-09 03:02:41 +1000

    add gear requirements to raids

--- src/lib/util/getOSItem.ts
+++ src/lib/util/getOSItem.ts
@@ -2,8 +2,10 @@ import { Items } from 'oldschooljs';
 import cleanItemName from './cleanItemName';
 import { Item } from 'oldschooljs/dist/meta/types';
 
-export default function getOSItem(itemName: string): Item {
-	const osItem = Items.get(cleanItemName(itemName)) as Item | undefined;
-	if (!osItem) throw `That item doesnt exist.`;
+export default function getOSItem(item: string | number): Item {
+	const osItem = Items.get(typeof item === 'string' ? cleanItemName(item) : item) as
+		| Item
+		| undefined;
+	if (!osItem) throw `'${item}' isn't a valid item.`;
 	return osItem;
 }
